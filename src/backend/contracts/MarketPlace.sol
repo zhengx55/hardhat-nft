cae637e136d5d97661b3725396ecc722e18ee50c
zhengx55 <williamzhxin@gmail.com>
2022-05-02 20:55:40 +0800

    first commit

---
+++ src/backend/contracts/MarketPlace.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Marketplace is ReentrancyGuard {
+    address payable public immutable feeAccount;
+    uint public immutable feePercent;
+    uint itemCount;
+
+    struct Item {
+        uint itemId;
+        IERC721 nft;
+        uint tokenId;
+        uint price;
+        address payable seller;
+        bool sold;
+    }
+    mapping(uint => Item) public items;
+
+    constructor(uint _feePercent) {
+        feeAccount = payable(msg.sender);
+        feePercent = _feePercent;
+    }
+
+    function makeItem(
+        IERC721 _nft,
+        uint _tokenId,
+        uint _price
+    ) external nonReentrant {}
+}
